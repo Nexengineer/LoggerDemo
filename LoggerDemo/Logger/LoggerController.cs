0f88591adf83d5e732f4a7be57737fa288c171b3
Neeraj Kumar Mishra <nexengineer@gmail.com>
2021-01-28 13:17:27 +0530

    first commit

---
+++ LoggerDemo/Logger/LoggerController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace LoggerDemo.Logger
+{
+    public class LoggerController
+    {
+        private readonly ILogger<LoggerController> logger;
+
+        public LoggerController(ILogger<LoggerController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [FunctionName("Demo")]
+        public async Task<IActionResult> PostConsent(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "logger")]
+            HttpRequest request)
+        {
+            logger.LogInformation($"Demo logger Information: {DateTime.Now}");
+            logger.LogError($"Demo logger Error: {DateTime.Now}");
+            logger.LogCritical($"Demo logger Critical: {DateTime.Now}");
+            return new AcceptedResult();
+        }
+    }
+}
---
+++ LoggerDemo/Startup.cs
@@ -0,0 +1,66 @@
+using System;
+using System.IO;
+using LoggerDemo;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.WebJobs.Host.Bindings;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NLog;
+using NLog.Extensions.Logging;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace LoggerDemo
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            InitWithLogger(builder);
+            InitConfig(builder);
+            InitDependencies(builder);
+        }
+
+        private static void InitWithLogger(IFunctionsHostBuilder builder)
+        {
+            var executionContextOptions = builder.Services.BuildServiceProvider()
+                .GetService<IOptions<ExecutionContextOptions>>().Value;
+            var currentDirectory = executionContextOptions.AppDirectory;
+            
+            LogManager.Setup()
+                .SetupExtensions(e => e.AutoLoadAssemblies(false))
+                .LoadConfigurationFromFile(currentDirectory + Path.DirectorySeparatorChar + "nlog.config", optional: false)
+                .LoadConfiguration(configurationBuilder => configurationBuilder.LogFactory.AutoShutdown = false);
+        }
+
+        private static void InitDependencies(IFunctionsHostBuilder builder)
+        {
+            builder.Services
+                .AddHttpClient()
+                .AddLogging((loggingBuilder) =>
+                {
+                    loggingBuilder.AddNLog(new NLogProviderOptions() { ShutdownOnDispose = true });
+                })
+                .AddMvc();
+        }
+
+        private static void InitConfig(IFunctionsHostBuilder builder)
+        {
+            var appEnv = Environment.GetEnvironmentVariable("APP_ENV");
+
+            var isLocal = appEnv == null || appEnv.Equals("local", StringComparison.OrdinalIgnoreCase);
+
+            var appDirectory = isLocal
+                ? Environment.CurrentDirectory
+                : $"{Environment.GetEnvironmentVariable("SITE_FOLDER")}/site/wwwroot";
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath($"{appDirectory}/ConfigFiles")
+                .AddJsonFile("appsettings.json", true)
+                .AddEnvironmentVariables()
+                .Build();
+            
+            builder.Services.AddOptions();
+        }
+    }
+}
